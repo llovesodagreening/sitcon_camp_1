6ed96469e07325b48244162edf4a5890d129d2f2
DaLun <excfergodst@gmail.com>
2016-08-04 17:52:00 +0800

    Hackathon Prepare

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ var express = require('express');
 
 var app = express();
 
-app.listen (8001, function(err){
+app.listen (4444, function(err){
 
 	console.log('Listening Port 8001');
 });
 
+app.use('/',express.static(__dirname));
 
 app.get('/',function(req, res) {
 	
 	res.sendFile(__dirname + '/web/index.html' , 'UTF-8');
+
+});
+
+app.get('/notified.js',function(req, res) {
+	
+	res.sendFile(__dirname + '/web/notified.js' , 'UTF-8');
 
 });
 
@@ -21,5 +28,5 @@ app.get('/other',function(req, res) {
 });
 
 app.use(function(req, res){
-		res.redirect('/');
+	res.redirect('/');
 });
---
+++ web/notified.js
@@ -0,0 +1,14 @@
+function desktop_notification(title ,body ,icon_str){
+
+    document.addEventListener('DOMContentLoaded',function(){
+
+        Notification.requestPermission(function(permission){
+              var config = {
+                      body:body,
+                      icon:icon_str,
+                      dir:'auto' 
+                  };
+             var notification = new Notification(title,config);
+         });
+    });
+}
