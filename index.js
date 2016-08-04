3d312efce81e299e9c69dfb0ddf56301cb58126d
DaLun <excfergodst@gmail.com>
2016-08-04 14:04:12 +0800

    simple route add

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ app.listen (8001, function(err){
 	console.log('Listening Port 8001');
 });
 
+
 app.get('/',function(req, res) {
-	res.sendFile(__dirname + '/web/index.html' , 'UTF-8');
+	
+	res.sendFile(__dirname + 'web/index.html' , 'UTF-8');
+
+});
+
+app.get('/other',function(req, res) {
+
+	res.sendFile(__dirname + 'web/other.html' , 'UTF-8');	
+
+});
+
+app.use(function(req, res){
+	res.redirect('/');
 });
