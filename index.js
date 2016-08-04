4c4835297de5e38d652d67eefb77a32da04138e2
DaLun <excfergodst@gmail.com>
2016-08-04 14:20:13 +0800

    miss / adain = =

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var express = require('express');
 
 var app = express();
 
-
-
 app.listen (8001, function(err){
 
 	console.log('Listening Port 8001');
@@ -16,12 +14,7 @@ app.get('/',function(req, res) {
 
 });
 
-app.get('/other',function(req, res) {
-
-	res.sendFile(__dirname + '/web/other.html' , 'UTF-8');	
-
-});
-
 app.use(function(req, res){
-	res.redirect('/');
+	res.write('error');
+	res.end();
 });
